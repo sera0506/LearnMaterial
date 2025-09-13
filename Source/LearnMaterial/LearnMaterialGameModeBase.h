07ba073916f0ebd72ddb44d0c6bc3c62d5f84fed
sera0506 <lakito1258@gmail.com>
2025-09-13 14:11:23 +0800

    MasterMaterial UV Control
    將完整的UV Control功能加入到material中
    並精簡為一個4 vector來進行tile和offset功能
    Add complete UV control functionality to the material
    and provides a 4-vector for tiling and offset functions

---
+++ Source/LearnMaterial/LearnMaterialGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearnMaterialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNMATERIAL_API ALearnMaterialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
